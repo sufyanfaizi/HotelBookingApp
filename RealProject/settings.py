0a227af382ace94adbbdb2a1abc4434c9d799a66
sufyanfaizi <sufyanfaizi456@gmail.com>
2019-09-28 01:02:59 +0500

    han bhai

--- RealProject/settings.py
+++ RealProject/settings.py
@@ -15,7 +15,6 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -129,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 
 #Extra places for collectstatic to find static files.
 #STATICFILES_DIRS = (
