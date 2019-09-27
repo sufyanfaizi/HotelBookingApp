9cc50d9ce9ae6317099be13c1a8bbab768f7f551
sufyanfaizi <sufyanfaizi456@gmail.com>
2019-09-28 00:41:41 +0500

    han bhai

--- RealProject/settings.py
+++ RealProject/settings.py
@@ -130,8 +130,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (
- #   os.path.join(PROJECT_ROOT, 'static'),
-#)
+#Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
