07655f5908ae6daeea1e22532d41d6152321f725
sufyanfaizi <sufyanfaizi456@gmail.com>
2019-09-28 11:33:54 +0500

    make it better

--- RealProject/settings.py
+++ RealProject/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = (
  #   os.path.join(BASE_DIR, 'static'),
   #  )
+ 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
