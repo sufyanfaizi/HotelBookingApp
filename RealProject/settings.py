8a689c1e9c5151a333c5a983f27d2233696c5df6
sufyanfaizi <sufyanfaizi456@gmail.com>
2019-09-28 12:38:34 +0500

    make it better

--- RealProject/settings.py
+++ RealProject/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 
 #Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'RealTodo/static'),
     )
  
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
