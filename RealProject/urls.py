c0b0d65367a8058a29f4cc06995dd9181c6f6a37
sufyanfaizi <sufyanfaizi456@gmail.com>
2019-09-29 12:55:37 +0500

    make it better

--- RealProject/urls.py
+++ RealProject/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('App/', include('RealTodo.urls')),
+    path('', include('RealTodo.urls')),
     path('accounts/', include('Accounts.urls')),
 
 
--- RealTodo/urls.py
+++ RealTodo/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('Home/<int:pk>/', views.tasksall , name='tasksall'),
     #path('hotel_booking/', views.hotel_booking , name='hotel_booking'),
     #path('hotel_booking/home/<int:pk>/', views.booking_details , name='booking_details'),
-    path('hotel_booking/home/', views.home , name='home'),
+    #path('hotel_booking/home/', views.home , name='home'),
     path('hotel_booking/AboutUs/', views.AboutUs , name='AboutUs'),
     path('hotel_booking/Details/', views.Details , name='Details'),
     path('hotel_booking/Details/<int:pk>/', views.booking_details , name='booking_details'),
     path('hotel_booking/Pricing/', views.Pricing , name='Pricing'),
-    #path('', views.home , name='home'),
+    path('', views.home , name='home'),
 
 
 ]
