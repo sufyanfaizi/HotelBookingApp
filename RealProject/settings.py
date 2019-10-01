7737f03ad30d57a02a62e14853385dea50be058a
sufyanfaizi <sufyanfaizi456@gmail.com>
2019-10-01 16:41:09 +0500

    make it better

--- RealProject/settings.py
+++ RealProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rglj+dnz!*(@p287q^x=y!m)iso(63wk##vp67@rr%1_)s^$7a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -131,7 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 #Extra places for collectstatic to find static files.
-
+#STATICFILES_DIRS = (
+ #   os.path.join(BASE_DIR, 'Realtodo/static'),
+  #  )
  
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
